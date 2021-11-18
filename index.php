9b643303dba88ea7f4cd781fe2e2fc096237ba15
Ebenezer Barinda <Ebenezer.Barinda@peoplelinkgroup.com>
2021-11-18 15:05:23 -0500

    Now I can pull songs from database

--- index.php
+++ index.php
@@ -1,36 +1,28 @@
-<?php
-include("includes/config.php");
+<?php include("includes/header.php")?>
+<h1 class="pageHeadingBig">You Might Also Like</h1>
 
-//session_destroy();
+<div class="gridViewContainer">
 
-if(isset($_SESSION['userLoggedIn'])){
-    $userLoggedIn = $_SESSION['userLoggedIn'];
-}
-else {
-    header("Location: register.php");
-}
+	<?php
+		$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
 
-?>
+		while($row = mysqli_fetch_array($albumQuery)) {
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to CloSpotify</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
-<body>
-	<div id="mainContainer">
+			echo "<div class='gridViewItem'>
 
-	<div id="topContainer">
+					<img src='" . $row['artworkPath'] . "'>
 
-		<?php include("includes/navBarContainer.php")?>
+					<div class='gridViewInfo'>"
+						. $row['title'] .
+					"</div>
 
-	</div>
-    
-		<?php include("includes/nowPlayingBar.php")?>
+				</div>";
 
-</body>
-</html>
+		}
+	?>
+
+</div>
+
+
+
+<?php include("includes/footer.php")?>
